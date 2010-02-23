aa4aa55f4ff2d1957d0ed78b90582865805b33ed
Philipp Meier <philipp@meier.name>
2010-02-23 20:34:47 +0100

    Add lein-clojars to dev-dependencies and fix project name

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject leiningen "0.1.0" 
+(defproject clj-validate "0.1.0" 
   :description "A Clojure library for declaring validators"
   :dependencies [[org.clojure/clojure "1.1.0"] 
 		 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.1.0"]])
+  :dev-dependencies [[swank-clojure "1.1.0"]
+		     [lein-clojars "0.5.0-SNAPSHOT"]])
